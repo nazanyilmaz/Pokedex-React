369f1467fbbe7c2f25d1900de8337f10ce34666b
Nazan Yilmaz <nazoo@Nazans-MacBook-Air.local>
2024-01-28 13:55:02 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Pokegame from "./Pokegame";
+import "./Pokecard.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Pokegame />
+    </div>
+  );
+}
+
+export default App;
